47745b359bb69b6f576be8ba14e8d9e92e3cf6ca
theamareshkar <1607152@kiit.ac.in>
2022-07-04 12:19:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from "./pages/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Suspense } from "react";
+import Admin from "./pages/Admin";
+// const Admin = React.lazy(()=> import("./pages/Admin"))
+import RequireAuth from "./component/RequireAuth";
+import Engineer from "./pages/Engineer";
+import Customer from "./pages/Customer";
+import NotFound from "./component/NotFound";
+import Unauthorized from './component/Unauthorised';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import '@coreui/coreui/dist/css/coreui.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
+import 'react-circular-progressbar/dist/styles.css';
+
+
+const ROLES = {
+  "CUSTOMER": "CUSTOMER",
+  "ENGINEER": "ENGINEER",
+  "ADMIN": "ADMIN",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Login />
+            </Suspense>
+          }
+        />
+        <Route path="unauthorized" element={<Unauthorized />} />
+        {/* ROLES.ADMIN ====  [ADMIN] */}
+          <Route element={<RequireAuth allowedRoles={[ROLES.ADMIN]} />}>  
+            <Route path="/admin" exact element={<Admin />} />
+          </Route>
+          {/* ROLES>CUSTOER === [CUSTOMER]   */}
+          <Route element={<RequireAuth allowedRoles={[ROLES.CUSTOMER]} />}>
+            <Route path="/customer" element={<Customer />} />
+          </Route>
+
+          {/* ROLES.ENINEER === ENGINEER */}
+          <Route element={<RequireAuth allowedRoles={[ROLES.ENGINEER]} />}>
+            <Route path="/engineer" element={<Engineer />} />
+          </Route>
+          <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
